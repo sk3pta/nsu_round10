497bedf4fffbc0a0b1e703d7039ac4148e0b223a
sk3pta <malopomalu9@gmail.com>
2022-11-11 18:28:07 +0300

    нихуя не работает

--- task3/main.c
+++ task3/main.c
@@ -14,8 +14,8 @@ return ((num & 0xff000000) >> 24) |
 
 void parseFieldName(FILE* inpfile, FILE* outfile) {
 	char* fieldName;	
-	fieldName = malloc(100);
-	char Ch = 0xFF;
+	fieldName = malloc(250);
+	int Ch = 0xFF;
 	while (Ch != '\0') 
 		{
 			fread(&Ch,1, 1, inpfile);
@@ -37,9 +37,8 @@ int main() {
 	
 
 
-
-	inpfileptr = fopen("testinp.bin","rb");
-	outfileptr = fopen("output.json","w");
+	inpfileptr = fopen("input.bin","rb");
+	outfileptr = fopen("output.json","wb");
 	fwrite("{\n", sizeof('{'), 1, outfileptr);
 	int32_t fileSize;
 	fread(&fileSize, sizeof(int32_t), 1, inpfileptr);
@@ -49,9 +48,41 @@ int main() {
 	{
 		fread(&T, 1, 1, inpfileptr);
 		
-		if ((int)T == 2) continue;	
+		
+	if ((int)T == 8) {
+		char value;
+		parseFieldName(inpfileptr, outfileptr);
+		fread(&value, 1, 1, inpfileptr);
 
-		if ((int)T == 1) 
+		if (value == 0) {
+		fprintf(outfileptr, "false");
+		}
+		else {
+		fprintf(outfileptr, "true");
+		}
+
+
+	}	
+
+
+	else	if ((int)T == 2) {
+			int* string = malloc(250);
+			parseFieldName(inpfileptr, outfileptr);
+				char Ch = 0xFF;
+			while (Ch != '\0') 
+			{
+				fread(&Ch,1, 1, inpfileptr);
+			   
+				strncat(string,&Ch,1);
+			}	
+			fprintf(outfileptr, " \"%s\" ", string);
+			free(string);
+
+		
+		
+		}	
+
+		else if ((int)T == 1) 
 		{
 		        double doubleValue = 0.0;	
 			parseFieldName(inpfileptr, outfileptr);	
@@ -63,7 +94,15 @@ int main() {
 			fprintf(outfileptr, " %.2lf", doubleValue);
 		}
 		
-		if ((int)T == 16)
+	
+		else if ((int)T == 10)
+		{
+			parseFieldName(inpfileptr, outfileptr);
+			fprintf(outfileptr, "null");
+		}
+
+		
+		else if ((int)T == 16)
 		{
 		
 			int32_t int32Value = 0;
@@ -73,7 +112,7 @@ int main() {
 		}
 
 
-		if ((int)T == 18)
+		else if ((int)T == 18)
 		{
 		
 			int64_t int64Value = 0;
